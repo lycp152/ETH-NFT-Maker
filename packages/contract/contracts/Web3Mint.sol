646db70053a155b652562b712bb28c0369db05bf
lycp152 <118104209+lycp152@users.noreply.github.com>
2023-10-25 12:14:16 +0900

    feat: S1-L3まで

---
+++ packages/contract/contracts/Web3Mint.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+import "hardhat/console.sol";
+contract Web3Mint {
+    constructor() {
+        console.log("This is my NFT contract.");
+    }
+}
