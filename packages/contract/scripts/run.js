f15b6c37ca0a63a92b59c740494f09fc9db41165
lycp152 <118104209+lycp152@users.noreply.github.com>
2023-10-25 12:44:25 +0900

    feat: S1-L4まで

--- packages/contract/scripts/run.js
+++ packages/contract/scripts/run.js
@@ -7,6 +7,14 @@ const main = async () => {
   // コントラクトが Mint され、ローカルのブロックチェーンにデプロイされるまで待ちます。
   await nftContract.deployed();
   console.log("Contract deployed to:", nftContract.address);
+  // makeAnEpicNFT 関数を呼び出す。NFT が Mint される。
+  let txn = await nftContract.makeAnEpicNFT();
+  // Minting が仮想マイナーにより、承認されるのを待つ。
+  await txn.wait();
+  // makeAnEpicNFT 関数をもう一度呼び出す。NFT がまた Mint される。
+  txn = await nftContract.makeAnEpicNFT();
+  // Minting が仮想マイナーにより、承認されるのを待つ。
+  await txn.wait();
 };
 
 // エラー処理を行っています。
