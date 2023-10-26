995ff719b18e1d93d097b6cea3e180794e363bc3
lycp152 <118104209+lycp152@users.noreply.github.com>
2023-10-26 15:34:49 +0900

    chore: Solidityのバージョンの修正

--- packages/contract/contracts/Web3Mint.sol
+++ packages/contract/contracts/Web3Mint.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 //OpenZeppelinが提供するヘルパー機能をインポートします。
